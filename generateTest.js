8a49ef034d9c70f86b5df879e6fd8d23797e0b92
Marcus Stüben <marcus@it-stueben.de>
2024-02-09 15:12:34 +0100

    initial commit

---
+++ generateTest.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { generate } from 'csv';
+import fs from 'fs';
+
+yargs(hideBin(process.argv))
+  .command(
+    'full',
+    'generate csv file with full info',
+    (yargs) => {
+      return yargs.option('count', {
+        alias: 'c',
+        default: 10,
+      });
+    },
+    (args) => {
+      generate({
+        columns: ['int', () => 'mydata'],
+        length: args.count,
+      }).pipe(process.stdout);
+    },
+  )
+  .demandCommand(1)
+  .help()
+  .parse();
